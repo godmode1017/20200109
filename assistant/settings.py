805d3530be46be2ad3062c4cbd64e43244387347
godmode1017 <kenny@barcorna.com.tw>
2020-01-09 12:16:32 +0800

    20200109

--- assistant/settings.py
+++ assistant/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'journal',
+    'expenses',
 ]
 
 MIDDLEWARE = [
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 ## Internationalization
-## https://docs.djangoproject.com/en/2.1/topics/i18n/
+## https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'zh-hant'
 
